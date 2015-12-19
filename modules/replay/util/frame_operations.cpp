2e86da658dddfe803703d1f2713982dd6d46b708
Krzysztof Pyrkosz <pyrkosz@o2.pl>
2015-12-19 12:57:14 +0100

    Replay: SSE optimized blend_images (disabled)

--- modules/replay/util/frame_operations.cpp
+++ modules/replay/util/frame_operations.cpp
@@ -30,6 +30,7 @@
 
 #define OPTIMIZE_RGB_TO_BGRA
 #define OPTIMIZE_BGRA_TO_RGB
+//#define OPTIMIZE_BLEND_IMAGES
 
 namespace caspar { namespace replay {
 
@@ -203,6 +204,7 @@ l1:
 	{
 		uint32_t full_size = width * height * stride;
 		uint16_t level_16 = (uint16_t)level;
+#ifndef OPTIMIZE_BLEND_IMAGES
 		tbb::parallel_for(tbb::blocked_range<size_t>(0, full_size), [=](const tbb::blocked_range<size_t>& r)
 		{
 			for (auto i = r.begin(); i != r.end(); i++)
@@ -210,6 +212,62 @@ l1:
 				dst[i] = (uint8_t)((((int)src1[i] * level_16) >> 6) + (((int)src2[i] * (64 - level_16)) >> 6));
 			}
 		});
+#else
+		const __m64* in1_vec = (__m64*)src1;
+		const __m64* in2_vec = (__m64*)src2;
+		__m64* out_vec = (__m64*)dst;
+		__m128i mul = _mm_set_epi16(level_16, level_16, level_16, level_16, level_16, level_16, level_16, level_16);
+		__m128i in1, in2;
+		__m128i mask = _mm_set_epi8(0x80, 7, 0x80, 6, 0x80, 5, 0x80, 4, 0x80, 3, 0x80, 2, 0x80, 1, 0x80, 0);
+		__m128i umask = _mm_set_epi8(0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 14, 12, 10, 8, 6, 4, 2, 0);
+
+		full_size /= 24;
+		while (full_size-- > 0)
+		{
+			/*             01 23 45 67 89 AB CD EF
+			 * in_vec[0]   Ra Ga Ba Rb Gb Bb Rc Gc     
+			 * in_vec[1]   Bc Rd Gd Bd Re Ge Be Rf 
+			 * in_vec[2]   Gg Bg Rh Gh Bh Ri Gi Bi
+			 */
+
+			in1 = _mm_loadl_epi64((__m128i*)&in1_vec[0]);
+			in2 = _mm_loadl_epi64((__m128i*)&in2_vec[0]);
+			in1 = _mm_shuffle_epi8(in1, mask);
+			in2 = _mm_shuffle_epi8(in2, mask);
+			in1 = _mm_sub_epi16(in1, in2);
+			in1 = _mm_mullo_epi16 (in1, mul);
+			in1 = _mm_srli_epi16(in1, 6);
+			in1 = _mm_add_epi16(in1, in2);
+			in1 = _mm_shuffle_epi8(in1, umask);
+			_mm_storel_epi64((__m128i*)&out_vec[0], in1);
+
+			in1 = _mm_loadl_epi64((__m128i*)&in1_vec[1]);
+			in2 = _mm_loadl_epi64((__m128i*)&in2_vec[1]);
+			in1 = _mm_shuffle_epi8(in1, mask);
+			in2 = _mm_shuffle_epi8(in2, mask);
+			in1 = _mm_sub_epi16(in1, in2);
+			in1 = _mm_mullo_epi16 (in1, mul);
+			in1 = _mm_srli_epi16(in1, 6);
+			in1 = _mm_add_epi16(in1, in2);
+			in1 = _mm_shuffle_epi8(in1, umask);
+			_mm_storel_epi64((__m128i*)&out_vec[1], in1);
+
+			in1 = _mm_loadl_epi64((__m128i*)&in1_vec[2]);
+			in2 = _mm_loadl_epi64((__m128i*)&in2_vec[2]);
+			in1 = _mm_shuffle_epi8(in1, mask);
+			in2 = _mm_shuffle_epi8(in2, mask);
+			in1 = _mm_sub_epi16(in1, in2);
+			in1 = _mm_mullo_epi16 (in1, mul);
+			in1 = _mm_srli_epi16(in1, 6);
+			in1 = _mm_add_epi16(in1, in2);
+			in1 = _mm_shuffle_epi8(in1, umask);
+			_mm_storel_epi64((__m128i*)&out_vec[2], in1);
+
+			in1_vec += 3;
+			in2_vec += 3;
+			out_vec += 3;
+		}
+#endif
 	}
 
 	void black_frame(mmx_uint8_t* dst, size_t width, size_t height, size_t stride)
